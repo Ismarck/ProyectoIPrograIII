25680c7558757d74f8a7a49021b0e1a047147207
Ismarck <arayaabarcamarcos@gmail.com>
2025-09-04 15:31:22 -0600

    Carga Inicial

---
+++ Documents/II ciclo 2025/programacion III/ProyectoIPrograIII/src/main/java/Modelo/Ejercicio.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author marcosisaacarayaabarca
+ */
+public class Ejercicio {
+
+    private String Nombre;
+    private String Area_de_Ejercicio;
+    private String Descripcion;
+    
+    public Ejercicio() {
+    }
+
+    public Ejercicio(String Nombre, String Area_de_Ejercicio, String Descripcion) {
+        this.Nombre = Nombre;
+        this.Area_de_Ejercicio = Area_de_Ejercicio;
+        this.Descripcion = Descripcion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getArea_de_Ejercicio() {
+        return Area_de_Ejercicio;
+    }
+
+    public void setArea_de_Ejercicio(String Area_de_Ejercicio) {
+        this.Area_de_Ejercicio = Area_de_Ejercicio;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Ejercicio{" + "Nombre=" + Nombre + ", Area_de_Ejercicio=" + Area_de_Ejercicio + ", Descripcion=" + Descripcion + '}';
+    }
+}
