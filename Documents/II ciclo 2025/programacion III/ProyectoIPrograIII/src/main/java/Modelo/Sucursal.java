b1a2f0943f7b8d76a02fda51d226e497b9c9c5e3
Ismarck <arayaabarcamarcos@gmail.com>
2025-09-03 23:33:18 -0600

    Carga Inicial

--- Documents/II ciclo 2025/programacion III/ProyectoIPrograIII/src/main/java/Modelo/Sucursal.java
+++ Documents/II ciclo 2025/programacion III/ProyectoIPrograIII/src/main/java/Modelo/Sucursal.java
@@ -9,8 +9,48 @@ package Modelo;
  * @author marcosisaacarayaabarca
  */
 public class Sucursal {
+
     private String Provincia;
     private String Canton;
     private int Codigo;
+    
+    public Sucursal() {
+    }
+
+    public Sucursal(String Provincia, String Canton, int Codigo) {
+        this.Provincia = Provincia;
+        this.Canton = Canton;
+        this.Codigo = Codigo;
+    }
+
+    public String getProvincia() {
+        return Provincia;
+    }
+
+    public void setProvincia(String Provincia) {
+        this.Provincia = Provincia;
+    }
+
+    public String getCanton() {
+        return Canton;
+    }
+
+    public void setCanton(String Canton) {
+        this.Canton = Canton;
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Sucursal{" + "Provincia=" + Provincia + ", Canton=" + Canton + ", Codigo=" + Codigo + '}';
+    }
+
     
 }
